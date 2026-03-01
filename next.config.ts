56e790bd57770ca2bf10f3bca6b20634819c2cdd
TheUtQ <utkuulsoy2004@gmail.com>
2026-03-01 03:52:27 +0300

    fix(next): allow i.ibb.co domain for images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
     unoptimized: false,
   },
   serverExternalPackages: ['better-sqlite3'],
